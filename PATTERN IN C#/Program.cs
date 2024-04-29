2b59a11561e9ff737c1d92533074c5610d8bfb55
hp <hp@DESKTOP-CROQ9O6>
2024-04-29 13:04:23 +0530

    Add project files.

---
+++ PATTERN IN C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PATTERN_IN_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers =  new int[3,3];
+
+            for (int i = 0; i < 3  ; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if(i == j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+                   
+                }
+                Console.WriteLine(); // MAKE NEW LINE
+            }
+            Console.ReadLine();
+        }
+    }
+}
